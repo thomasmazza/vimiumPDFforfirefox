d2bb30b085dc8c927095718258e43d4c2b4bf128
Thomas Mazza <thomas.mazza@rwth-aachen.de>
2023-04-13 09:47:51 +0200

    Change destination folder name

--- script/install.py
+++ script/install.py
@@ -39,7 +39,7 @@ if is_admin():
     program_files = os.environ.get('ProgramFiles')
 
     # Specify the target folder name inside the Program Files folder
-    destination_folder_name = "YourTargetFolderName"
+    destination_folder_name = "VimiumForFirefox"
     destination_path = os.path.join(program_files, destination_folder_name)
 
     print(f"Source: {source}")  # Print source path
