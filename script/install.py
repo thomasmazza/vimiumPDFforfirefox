c168518d1cc160f77000ba702eb14d20be7ec2c3
Thomas Mazza <thomas.mazza@rwth-aachen.de>
2023-04-15 22:57:01 +0200

    Refactor setup_folder and install_dependencies

--- script/install.py
+++ script/install.py
@@ -16,24 +16,42 @@ except ImportError:
 config = configparser.ConfigParser()
 config.read('config.ini')
 
+PROGRAM_FILES = os.environ.get('ProgramFiles')
 DESTINATION_FOLDER_NAME = config.get('general', 'destination_folder_name')
+DESTINATION_PATH = os.path.join(PROGRAM_FILES, DESTINATION_FOLDER_NAME)
 PDF_FOLDER_NAME = config.get('general', 'pdf_folder_name')
 SERVER_PORT = config.getint('server', 'port')
 AUTOSTART = config.getboolean('general', 'autostart')
+SCRIPT_PATH = os.path.abspath(__file__)
+SOURCE = os.path.dirname(os.path.dirname(SCRIPT_PATH))
+TEMP_FOLDER = os.environ.get("TEMP")
+PDF_FOLDER_PATH = os.path.join(TEMP_FOLDER, PDF_FOLDER_NAME)
+PDF_FILES_FOLDER = "pdf"
+PDF_FOLDER_SYMLINK_PATH = os.path.join(DESTINATION_PATH, PDF_FILES_FOLDER)
 
 
 def create_shortcut(target, path, description):
-    try:
-        shell = Dispatch('WScript.Shell')
-        shortcut = shell.CreateShortcut(path)
-        shortcut.TargetPath = "runas.exe"
-        shortcut.Arguments = f'/savecred /user:Administrator "{target}"'
-        shortcut.Description = description
-        shortcut.WorkingDirectory = os.path.dirname(target)
-        shortcut.IconLocation = target
-        shortcut.save()
-    except Exception as e:
-        print(f"Error occurred while creating the shortcut: {e}")
+    shell = Dispatch('WScript.Shell')
+    shortcut = shell.CreateShortcut(path)
+    shortcut.TargetPath = "runas.exe"
+    shortcut.Arguments = f'/savecred /user:Administrator "{target}"'
+    shortcut.Description = description
+    shortcut.WorkingDirectory = os.path.dirname(target)
+    shortcut.IconLocation = target
+    shortcut.save()
+
+
+def install_dependencies():
+    if is_npm_installed():
+        print("npm is installed")
+        if not (is_gulp_installed()):
+            print("gulp-cli is not installed. Installing gulp-cli..")
+            install_gulp
+
+        os.chdir(DESTINATION_PATH)
+        run_npm_install()
+    else:
+        print("npm is not installed. Please install npm and try again.")
 
 
 def is_admin():
@@ -102,64 +120,38 @@ def install_gulp():
     try:
         subprocess.check_output("npm install -g gulp-cli", shell=True)
         print("gulp-cli has been installed globally.")
-    except subprocess.CalledProcessErrori as e:
+    except subprocess.CalledProcessError as e:
         print(f"Error eccoured while installing gulp: {e}")
+
+
+def setup_folders():
+    copytree(SOURCE, DESTINATION_PATH)
+    print("Copying completed.")
+    if not os.path.exists(PDF_FOLDER_PATH):
+        os.makedirs(PDF_FOLDER_PATH)
+    create_symlink(PDF_FOLDER_PATH, DESTINATION_PATH, PDF_FILES_FOLDER)
 
 
 # Main Routine
 if is_admin():
     print("The script is running with administrator privileges.")
 
-    script_path = os.path.abspath(__file__)
-    source = os.path.dirname(os.path.dirname(script_path))
-    program_files = os.environ.get('ProgramFiles')
+    install_dependencies()
+    setup_folders()
 
-    # Specify the target folder name inside the Program Files folder
-    destination_path = os.path.join(program_files, DESTINATION_FOLDER_NAME)
-
-    print(f"Source: {source}")
-    print(f"Destination: {destination_path}")
-
-    copytree(source, destination_path)
-    print("Copying completed.")
-
-    if is_npm_installed():
-        print("npm is installed.")
-        if not (is_gulp_installed()):
-            print("gulp-cli is not installed. Installing gulp-cli..")
-            install_gulp()
-
-        os.chdir(destination_path)
-        run_npm_install()
-
-        # Create Symlink to a TEMP Folder for the pdfs
-        temp_folder = os.environ.get("TEMP")
-        pdf_folder_path = os.path.join(temp_folder, PDF_FOLDER_NAME)
-        print("Before copying")
-        if not os.path.exists(pdf_folder_path):
-            os.makedirs(pdf_folder_path)
-            print(pdf_folder_path)
-
-        # Create pdf folder in Program Files for symlink
-        pdf_files_folder = "pdf"
-        pdf_folder_symlink_path = os.path.join(
-            destination_path, pdf_files_folder)
-
-        create_symlink(pdf_folder_path, destination_path, "pdf")
-
-        # Create Shortcut in Autostart
-        if (AUTOSTART):
-            batch_file = os.path.join(
-                os.environ['ProgramFiles'],
-                r'VimiumPDFForFirefox\script',
-                'run_gulp_server.bat')
-            startup_folder = os.path.join(
-                os.environ['APPDATA'],
-                r'Microsoft\Windows\Start Menu\Programs\Startup')
-            shortcut_path = os.path.join(startup_folder, 'GulpServerPDF.lnk')
-            create_shortcut(batch_file, shortcut_path,
-                            'Run Gulp server as administrator')
-            print(f'Shortcut created at: {shortcut_path}')
+    # Create Shortcut in Autostart
+    if (AUTOSTART):
+        batch_file = os.path.join(
+            os.environ['ProgramFiles'],
+            r'VimiumPDFForFirefox\script',
+            'run_gulp_server.bat')
+        startup_folder = os.path.join(
+            os.environ['APPDATA'],
+            r'Microsoft\Windows\Start Menu\Programs\Startup')
+        shortcut_path = os.path.join(startup_folder, 'GulpServerPDF.lnk')
+        create_shortcut(batch_file, shortcut_path,
+                        'Run Gulp server as administrator')
+        print(f'Shortcut created at: {shortcut_path}')
 
 else:
     print("The script is running without administrator privileges.")
